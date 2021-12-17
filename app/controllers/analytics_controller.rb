b7952f064e15386712d71f9a525e1bc8d121fdc1
noslopy <info@noslopy.dev>
2021-12-17 07:48:54 +0100

    Make output prettier

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,15 +1,16 @@
+require 'json'
 require './lib/analytics/page_visits_analyzer'
 
 class AnalyticsController < ActionController::Base
 
     def pages_by_pageviews
         pva = PageVisitsAnalyzer.new
-        render json: pva.pages_by_visitors
+        render json: JSON.pretty_generate(pva.pages_by_visitors)
     end
 
     def unique_pageviews
         pva = PageVisitsAnalyzer.new
-        render json: pva.unique_pageviews
+        render json: JSON.pretty_generate(pva.unique_pageviews)
     end
 
 end
